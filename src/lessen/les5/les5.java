a3e1e3ac2a4ff510eaeb5dcb4cd705b1cbcc7d66
thom <thom_peele@hotmail.com>
2016-09-23 11:04:45 +0200

    added balanceSymbolChecker

--- src/lessen/les5/les5.java
+++ src/lessen/les5/les5.java
@@ -25,7 +25,37 @@ public class les5 {
         System.out.println(hanStack.top());
         System.out.println(hanStack.pop());
         System.out.println(hanStack.pop());
+        balanceSymbolChecker("((([])))");
 
+
+    }
+
+    /**
+     * balanceSymbolChecker - given input from '(','[',')' or ']' it will check if the bracket things align with each other
+     * @param input
+     */
+    public static void balanceSymbolChecker(String input) {
+        char[] inputArray = input.toCharArray();
+        HANStack<Character> characterHANStack = new HANStack<Character>();
+
+        for(int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == '(' || inputArray[i] == '[') {
+                characterHANStack.push(inputArray[i]);
+            } else if(inputArray[i] == ')' || inputArray[i] == ']') {
+                if(characterHANStack.getSize() == 0) {
+                    throw new Error("Stack vroegtijdig leeg");
+                } else {
+                    if((inputArray[i] == ')' && characterHANStack.top() == '(') || (inputArray[i] == ']' && characterHANStack.top() == '[')) {
+                        System.out.println(characterHANStack.pop());
+                    } else {
+                        throw new Error("Haakjes corresponderen niet");
+                    }
+                }
+            }
+        }
+        if(characterHANStack.getSize() > 0) {
+            throw new Error("stack niet leeg");
+        }
     }
 
 }
